434d0ac5fb3313306adde9faf323ed9e1c37a417
kallevonbach <kallevonbach@gmail.com>
2018-06-21 08:47:41 +0200

    reupload

--- src/php/connect.php
+++ src/php/connect.php
@@ -1,8 +1,8 @@
 <?php 
     $host = "mysql24.unoeuro.com";
     $db = "casperbach_dk_db4";
-    $user = "casperbach_dk";
-    $password = "3L97Xis9gs";
+    $user = "anon";
+    $password = "nope";
     try {
         $dbh = new PDO("mysql:host=$host;dbname=$db", $user, $password);
     } catch (PDOException $ex) {
